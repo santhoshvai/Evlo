cbbc7d57ad4f026b5aa10e652a53437c9d1986b9
santhosh vaiyapuri <santhoshvai@gmail.com>
2017-05-19 18:46:05 +0200

    remove bottom bar and add search bar on top

--- app/src/main/java/info/santhosh/evlo/ui/main/MainActivity.java
+++ app/src/main/java/info/santhosh/evlo/ui/main/MainActivity.java
@@ -1,14 +1,10 @@
 package info.santhosh.evlo.ui.main;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.widget.Toast;
 
-import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
-import com.aurelhubert.ahbottomnavigation.AHBottomNavigationAdapter;
-import com.aurelhubert.ahbottomnavigation.AHBottomNavigationViewPager;
 import com.facebook.device.yearclass.YearClass;
 
 import info.santhosh.evlo.R;
@@ -18,19 +14,11 @@ import info.santhosh.evlo.ui.search.SearchToolbar;
  * Created by santhoshvai on 12/02/17.
  */
 
-public class MainActivity extends AppCompatActivity implements AHBottomNavigation.OnTabSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
     SearchToolbar searchToolbar;
-
-    // fragments switching
-    private AHBottomNavigationViewPager viewPager;
-    private FragmentStatePagerAdapter adapter;
-
-    // bottom bar related
-    private AHBottomNavigationAdapter navigationAdapter;
-    private AHBottomNavigation bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,45 +26,18 @@ public class MainActivity extends AppCompatActivity implements AHBottomNavigatio
         setContentView(R.layout.activity_main);
         Toast.makeText(this, Integer.toString(YearClass.get(getApplicationContext())), Toast.LENGTH_LONG).show();
 
-        // view pager
-        viewPager = (AHBottomNavigationViewPager) findViewById(R.id.view_pager);
-        viewPager.setOffscreenPageLimit(2);
-        adapter = new MainPagerAdapter(getSupportFragmentManager());
-        viewPager.setAdapter(adapter);
-
         // search bar
         searchToolbar = (SearchToolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(searchToolbar);
 
-        // bottom bar
-        bottomNavigation = (AHBottomNavigation) findViewById(R.id.bottom_bar);
-        bottomNavigation.setOnTabSelectedListener(this);
-        bottomNavigation.setAccentColor(ContextCompat.getColor(getApplicationContext(), R.color.teal_500));
-        bottomNavigation.setInactiveColor(ContextCompat.getColor(getApplicationContext(), R.color.inactive_bottombar));
-        navigationAdapter = new AHBottomNavigationAdapter(this, R.menu.bottom_bar_menu);
-        navigationAdapter.setupWithBottomNavigation(bottomNavigation);
     }
 
-    /**
-     * Called when a bottom bar tab has been selected (clicked)
-     *
-     * @param position    int: Position of the selected tab
-     * @param wasSelected boolean: true if the tab was already selected
-     * @return boolean: true for updating the tab UI, false otherwise
-     */
+    // Menu icons are inflated just as they were with actionbar
     @Override
-    public boolean onTabSelected(int position, boolean wasSelected) {
-
-        if(position == 0) searchToolbar.setTitle(R.string.search_commodities);
-        else if(position == 1) searchToolbar.setTitle(R.string.search_markets);
-        // TODO: hide search on position 3
-
-        if (wasSelected) {
-            return true;
-        }
-
-        viewPager.setCurrentItem(position, false);
-
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
 }
