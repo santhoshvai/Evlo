08d48e532f4cf229d3c48b617df94b0f905f4cfb
santhosh vaiyapuri <santhoshvai@gmail.com>
2017-05-26 17:14:51 +0200

    added additional common utils

--- app/src/main/java/info/santhosh/evlo/common/Utils.java
+++ app/src/main/java/info/santhosh/evlo/common/Utils.java
@@ -2,10 +2,14 @@ package info.santhosh.evlo.common;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Point;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.annotation.NonNull;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.TextView;
 
@@ -13,6 +17,9 @@ import android.widget.TextView;
  * Created by santhoshvai on 09/05/16.
  */
 public class Utils {
+
+    private static int screenWidth = 0;
+    private static int screenHeight = 0;
 
     private Utils() {}
 
@@ -70,6 +77,34 @@ public class Utils {
         } else { // search did not yield results
             mEmptyView.setText("No search results for " + searchFilter);
         }
+    }
+
+    public static int dpToPx(int dp) {
+        return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
+    }
+
+    public static int getScreenHeight(Context c) {
+        if (screenHeight == 0) {
+            WindowManager wm = (WindowManager) c.getSystemService(Context.WINDOW_SERVICE);
+            Display display = wm.getDefaultDisplay();
+            Point size = new Point();
+            display.getSize(size);
+            screenHeight = size.y;
+        }
+
+        return screenHeight;
+    }
+
+    public static int getScreenWidth(Context c) {
+        if (screenWidth == 0) {
+            WindowManager wm = (WindowManager) c.getSystemService(Context.WINDOW_SERVICE);
+            Display display = wm.getDefaultDisplay();
+            Point size = new Point();
+            display.getSize(size);
+            screenWidth = size.x;
+        }
+
+        return screenWidth;
     }
 
 }
